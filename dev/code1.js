9d49af6b3b642a0179db5cdab2d1610ab1652617
Caique-Sampaio <caique.sam93@gmail.com>
2025-05-15 16:16:40 -0300

    açteração na calculadora de somar

--- dev/code1.js
+++ dev/code1.js
@@ -17,7 +17,7 @@ function calculadora(operacao, num1, num2) {
 }
 
 // Exemplos de uso:
-console.log(calculadora('somar', 5, 3)); // 8
+console.log(calculadora('somar', 25, 3)); // 8
 console.log(calculadora('subtrair', 10, 2)); // 2
 console.log(calculadora('multiplicar', 10, 3)); // 15
 console.log(calculadora('dividir', 10, 2)); // Erro: divisão por zero
