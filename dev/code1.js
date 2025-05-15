7b9d9bac92ae1055ab2dc0b2575387c9e2a6e9a0
Caique-Sampaio <caique.sam93@gmail.com>
2025-05-15 16:01:31 -0300

    nova pasta de dev + codigo calculadora

---
+++ dev/code1.js
@@ -0,0 +1,23 @@
+function calculadora(operacao, num1, num2) {
+    switch (operacao) {
+        case 'somar':
+            return num1 + num2;
+        case 'subtrair':
+            return num1 - num2;
+        case 'multiplicar':
+            return num1 * num2;
+        case 'dividir':
+            if (num2 === 0) {
+                return 'Erro: divisão por zero';
+            }
+            return num1 / num2;
+        default:
+            return 'Operação inválida';
+    }
+}
+
+// Exemplos de uso:
+console.log(calculadora('somar', 5, 3)); // 8
+console.log(calculadora('subtrair', 5, 3)); // 2
+console.log(calculadora('multiplicar', 5, 3)); // 15
+console.log(calculadora('dividir', 5, 0)); // Erro: divisão por zero
